3894b4a66b13c5ceebb414b98d6cfb2243b686d4
slimkong25 <karthik25ayu@gmail.com>
2026-05-04 23:17:15 +0530

    update

---
+++ day2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 5;
+    if(i>=4){
+        cout << i << endl ; 
+    }else{
+        cout << i-4 << endl;
+    }
+
+    for(int i=0;i<5;i++){
+        cout << i << endl;
+    }
+
+    int j = 5;
+    while(j>0){
+        cout << j << endl;
+        j--;
+    }
+}
